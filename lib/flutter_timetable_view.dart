a704f19ed4b388ec89ee910893fef9617a4927d2
yamarkz <y.kazuki0614n@gmail.com>
2020-02-27 22:15:06 +0900

    add export style.dart

--- lib/flutter_timetable_view.dart
+++ lib/flutter_timetable_view.dart
@@ -4,4 +4,5 @@ export 'src/models/lane.dart';
 export 'src/models/lane_events.dart';
 export 'src/models/table_event.dart';
 export 'src/models/table_event_time.dart';
+export 'src/styles/timetable_style.dart';
 export 'src/views/timetable_view.dart';
