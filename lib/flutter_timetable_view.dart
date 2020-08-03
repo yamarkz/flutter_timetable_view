b01fc20b275b919426be27b0a2728e239582a3a2
みやけりょう <ryo@miyake.id>
2020-08-03 15:06:44 +0900

    export diagonal_scroll_view

--- lib/flutter_timetable_view.dart
+++ lib/flutter_timetable_view.dart
@@ -5,4 +5,6 @@ export 'src/models/lane_events.dart';
 export 'src/models/table_event.dart';
 export 'src/models/table_event_time.dart';
 export 'src/styles/timetable_style.dart';
+export 'src/views/controller/timetable_view_controller.dart';
+export 'src/views/diagonal_scroll_view.dart';
 export 'src/views/timetable_view.dart';
