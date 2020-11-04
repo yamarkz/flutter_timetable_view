eabadcecf5d29c7c1590bb3b2a33109eaa0613cb
tanmoy27112000 <tkarmakar27112000@gmail.com>
2020-11-04 17:03:28 +0530

    wip

--- lib/src/views/timetable_view.dart
+++ lib/src/views/timetable_view.dart
@@ -110,36 +110,54 @@ class _TimetableViewState extends State<TimetableView>
               i += 1)
             i
         ].map((hour) {
-          return Container(
-            height: widget.timetableStyle.timeItemHeight,
-            decoration: BoxDecoration(
-              // border: Border(
-              //   top: BorderSide(
-              //     color: widget.timetableStyle.timelineBorderColor,
-              //     width: 0,
-              //   ),
-              // ),
-              color: widget.timetableStyle.timelineItemColor,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  Utils.hourFormatter(hour > 12 ? hour - 12 : hour, 0)
-                              .substring(0, 2)[0] ==
-                          "0"
-                      ? Utils.hourFormatter(hour > 12 ? hour - 12 : hour, 0)
-                              .substring(1, 2) +
-                          " ${hour > 12 ? "PM" : "AM"}"
-                      : Utils.hourFormatter(hour > 12 ? hour - 12 : hour, 0)
-                              .substring(0, 2) +
-                          " ${hour > 12 ? "PM" : "AM"}",
-                  style: TextStyle(
-                      color: widget.timetableStyle.timeItemTextColor),
+          return Stack(
+            alignment: Alignment.topCenter,
+            children: [
+              Container(
+                height: widget.timetableStyle.timeItemHeight,
+                decoration: BoxDecoration(
+                  // border: Border(
+                  //   top: BorderSide(
+                  //     color: widget.timetableStyle.timelineBorderColor,
+                  //     width: 0,
+                  //   ),
+                  // ),
+                  color: widget.timetableStyle.timelineItemColor,
                 ),
-              ],
-            ),
+                // child: Row(
+                //   mainAxisAlignment: MainAxisAlignment.center,
+                //   crossAxisAlignment: CrossAxisAlignment.start,
+                //   children: [
+                //     Text(
+                //       Utils.hourFormatter(hour > 12 ? hour - 12 : hour, 0)
+                //                   .substring(0, 2)[0] ==
+                //               "0"
+                //           ? Utils.hourFormatter(hour > 12 ? hour - 12 : hour, 0)
+                //                   .substring(1, 2) +
+                //               " ${hour > 12 ? "PM" : "AM"}"
+                //           : Utils.hourFormatter(hour > 12 ? hour - 12 : hour, 0)
+                //                   .substring(0, 2) +
+                //               " ${hour > 12 ? "PM" : "AM"}",
+                //       style: TextStyle(
+                //           color: widget.timetableStyle.timeItemTextColor),
+                //     ),
+                //   ],
+                // ),
+              ),
+              Text(
+                Utils.hourFormatter(hour > 12 ? hour - 12 : hour, 0)
+                            .substring(0, 2)[0] ==
+                        "0"
+                    ? Utils.hourFormatter(hour > 12 ? hour - 12 : hour, 0)
+                            .substring(1, 2) +
+                        " ${hour > 12 ? "PM" : "AM"}"
+                    : Utils.hourFormatter(hour > 12 ? hour - 12 : hour, 0)
+                            .substring(0, 2) +
+                        " ${hour > 12 ? "PM" : "AM"}",
+                style:
+                    TextStyle(color: widget.timetableStyle.timeItemTextColor),
+              ),
+            ],
           );
         }).toList(),
       ),
