ceb7bf789f59402f91e8c32d8a7bec028fc45594
tanmoy27112000 <tkarmakar27112000@gmail.com>
2020-11-04 12:40:59 +0530

    wip

--- lib/src/views/timetable_view.dart
+++ lib/src/views/timetable_view.dart
@@ -154,7 +154,7 @@ class _TimetableViewState extends State<TimetableView>
         shrinkWrap: true,
         children: widget.laneEventsList.map((laneEvents) {
           return Container(
-            decoration: BoxDecoration(border: Border.all(color: Colors.red)),
+            decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
             width: laneEvents.lane.width,
             height: laneEvents.lane.height,
             // color: laneEvents.lane.backgroundColor,
