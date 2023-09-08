bff60d4aa06425639b20f47aa14cf40e0a852fcb
parisima-etemadi <parisimaetemadi9675@gmail.com>
2023-09-08 21:26:32 +0330

    change font

--- lib/src/utils/utils.dart
+++ lib/src/utils/utils.dart
@@ -77,7 +77,7 @@ class Utils {
         SizedBox(height: 5,),
         Text(
           event.price,
-          style: TextStyle(color: Colors.white60,fontSize: 5),
+          style: TextStyle(color: Colors.white60,fontSize: 10),
         ),
       ],
     );
